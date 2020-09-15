4c01d53091061fcf38c532a63debf07dd27c4833
bryantobing12 <bryantobing0@gmail.com>
2020-09-15 18:32:45 +0700

    update and mocking our test

--- tests/user.test.js
+++ tests/user.test.js
@@ -97,5 +97,39 @@ test('Should not delete account for unauthenticated user', async() => {
     await request(app)
         .delete('/users/me')
         .send()
-        .expect(401)
+        .expect(401);
+});
+
+test('Should upload avatar image', async() => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar','tests/fixtures/profile-pic.jpg')
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async() => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name : "Bryan"
+        })
+        .expect(200)
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toEqual('Bryan')
+});
+
+test('Should not update invalid user fields', async() => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location : 'Philadelphia'
+        })
+        .expect(400)
 });
